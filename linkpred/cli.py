1edabd4b32a9b00dbd9a44c6853fb075a89c85d9
Raf Guns <rafguns@gmail.com>
2013-10-10 17:11:26 +0200

    Rework CLI and core functions

--- linkpred/cli.py
+++ linkpred/cli.py
@@ -1,140 +1,98 @@
+import argparse
 import json
-from optparse import OptionParser
 
 from .predictors import all_predictors
 from .util import log
 
-__all__ = ["load_profile", "get_profile", "get_profile_by_options",
-           "options_n_args"]
+__all__ = ["load_profile", "get_profile", "handle_arguments"]
 
 
-def data_from_profile(fname):
-    data = {}
+class Profile(object):
+    pass
+
+
+def load_profile(fname):
+    profile = {}
     try:
         with open(fname) as f:
             if fname.endswith(".yaml"):
                 import yaml
-                data = yaml.safe_load(f)
+                profile = yaml.safe_load(f)
             else:
-                data = json.load(f)
+                profile = json.load(f)
     except (AttributeError, TypeError) as e:
-        log.logger.warning("Encountered error '%s'" % e)
+        log.logger.error("Encountered error '%s'" % e)
     finally:
-        return data
+        return profile
 
 
-def fancy_update(base, new):
-    updated = dict(base.iteritems())
-    for k, v in new.iteritems():
-        if k not in base:
-            updated[k] = v
-        elif type(base[k]) == type(v) == dict:
-            updated[k] = fancy_update(base[k], v)
-        elif type(base[k]) == type(v) == list:
-            updated[k].extend(v)
-        else:
-            updated[k] = v
-    return updated
+def get_profile():
+    args = handle_arguments()
+    profile = load_profile(args.profile) if args.profile else {}
 
-
-def load_profile(*fnames):
-    """
-    Load profile from one or more files
-
-    Arguments
-    ---------
-    fnames : one or more strings (file names)
-
-    """
-    profile = {}
-    for fname in fnames:
-        data = data_from_profile(fname)
-        profile = fancy_update(profile, data)
-    return profile
-
-
-def get_profile(**kwargs):
-    options, args = options_n_args(**kwargs)
-    if args:
-        log.logger.warning("Ignoring arguments: %s" % str(args))
-    return get_profile_by_options(options)
-
-
-def get_profile_by_options(options):
-    """Determine a profile based on available options
-
-    If multiple profiles are passed through the CLI interface,
-    they are merged into one. In case of conflicts, the last profile
-    supersedes the previous ones.
-    Other CLI options supersede the profiles.
-
-    Arguments
-    ---------
-
-    options : an optparse.Options object
-
-    Returns
-    -------
-
-    profile : a dict
-
-    """
-    profile = load_profile(*options.profile)
-
-    option_names = ["charts", "filetype", "interpolation", "steps", "only_new"]
-    for option_name in option_names:
+    arg_names = ["output", "chart_filetype", "interpolation", "only_new",
+                 "training", "test"]
+    for arg_name in arg_names:
         try:
-            option = getattr(options, option_name)
+            arg = getattr(args, arg_name)
         except AttributeError:
             continue
-        profile[option_name] = option
+        profile[arg_name] = arg
 
-    if hasattr(options, 'predictors') and options.predictors:
+    if hasattr(args, 'predictors') and args.predictors:
         profile['predictors'] = []
-        for p in options.predictors:
+        for p in args.predictors:
             profile['predictors'].append({'name': p})
 
     return profile
 
 
-def options_n_args(choose_chart=True, choose_profile=True,
-                   choose_predictor=True, choose_filetype=False,
-                   choose_weight=False, choose_interpolation=False):
-    """Get nice CLI interface and return options 'n arguments."""
+def handle_arguments():
+    """Get nice CLI interface and return arguments."""
 
-    parser = OptionParser()
-    parser.add_option("--debug", action="store_true", dest="debug",
-                      default=False, help="Log debug messages")
-    if choose_chart:
-        chart_help = "Type of chart(s) to produce (default: all available)."
-        chart_types = ["recall-precision", "F-score", "ROC"]
-        parser.add_option("-c", "--chart", help=chart_help, action="append",
-                          choices=chart_types, dest="charts", default=chart_types)
-    if choose_filetype:
-        parser.add_option("-f", "--filetype",
-                          help="Output file type (default: %default)", default="pdf")
-    if choose_interpolation:
-        parser.add_option("-i", "--no-interpolation",
-                          help="Do not interpolate precision", action="store_false",
-                          dest="interpolation", default=True)
-    if choose_predictor:
-        predictors = [p.__name__ for p in all_predictors()]
-        parser.add_option(
-            "-p", "--predictors", action="append", dest="predictors",
-            help="Predicting methods to use (default: all available)",
-            choices=predictors, default=[])
-        parser.add_option(
-            "-n", "--only-new", action="store_true", dest="only_new",
-            default=False,
-            help="Only consider new (unattested) predictions")
-    if choose_profile:
-        parser.add_option("-P", "--profile", action="append",
-                          help="JSON profile file", default=[])
+    parser = argparse.ArgumentParser(
+        version="0.1", description="Easy link prediction tool",
+        usage="%(prog)s [options] training [test]")
 
-    options, args = parser.parse_args()
-    if options.debug:
+    parser.add_argument("--debug", action="store_true", dest="debug",
+                        default=False, help="Show debug messages")
+    parser.add_argument("-q", "--quiet", action="store_true", dest="quiet",
+                        default=False, help="Don't show info messages")
+
+    output_types = ["recall-precision", "F-score", "ROC"]
+    output_help = "Type of output(s) to produce (default: %(default)s)"
+    parser.add_argument("-o", "--output", help=output_help, action="append",
+                        choices=output_types, default=output_types[0])
+
+    parser.add_argument("-f", "--chart-filetype", default="pdf",
+                        help="File type for charts (default: %(default)s)")
+
+    parser.add_argument("-i", "--interpolation",
+                        help="Interpolate recall-precision charts",
+                        action="store_true", default=False)
+
+    predictors = [p.__name__ for p in all_predictors()]
+    parser.add_argument("-p", "--predictor", action="append",
+                        dest="predictors", choices=predictors, default=[],
+                        help="Predictor(s) to use for link prediction")
+
+    all_help = "Predict all links, "\
+               "including ones present in the training network"
+    parser.add_argument("-a", "--all", action="store_false", dest="only_new",
+                        default=True, help=all_help)
+
+    parser.add_argument("-P", "--profile", help="JSON/YAML profile file")
+
+    parser.add_argument("training")
+    parser.add_argument("test", nargs="?")
+
+    results = parser.parse_args()
+
+    if results.debug:
         log.logger.setLevel(log.logging.DEBUG)
+    elif results.quiet:
+        log.logger.setLevel(log.logging.WARNING)
     else:
         log.logger.setLevel(log.logging.INFO)
 
-    return options, args
+    return vars(results)
--- linkpred/core.py
+++ linkpred/core.py
@@ -1,92 +1,110 @@
+import networkx as nx
 from . import predictors
-from .evaluation import Comparison
-from .result import ResultDict  # XXX
+from .evaluation import Comparison, signals, listeners
+from .network import read_pajek
 from .util import log
 
 
-def training_test_data(profile, minimum_degree=1, **kwargs):
-    endpoint = profile["sparql_endpoint"]
-    query = profile.get("query")
-    training = profile["training"]
-    test = profile["test"]
+def read_network(fname):
+    filetype_readers = {'net': read_pajek,
+                        'gml': nx.read_gml,
+                        'graphml': nx.read_graphml,
+                        'gexf': nx.read_gexf,
+                        'edgelist': nx.read_edgelist,
+                        'adjlist': nx.read_adjlist}
 
-    results = ResultDict()
-    for dataprofile in (training, test):
-        name = dataprofile["name"]
-        parameters = dataprofile["parameters"]
-        if query is None:
-            query = dataprofile["query"]
+    for ext, read in filetype_readers.iteritems():
+        if fname.lower().endswith(ext):
+            log.logger.info("Reading file '%s'..." % fname)
+            network = read(fname)
+            log.logger.info("Successfully read file.")
 
-        log.logger.info("Collecting data (%s)..." % name)
-        # XXX TODO XXX
-        results[name] = "TODO"
-        log.logger.info("Finished collecting data.")
-
-    if minimum_degree:
-        results.filter_all_low_degree_nodes(minimum_degree)
-    return results[training['name']], results[test['name']]
+    return network
 
 
-def pretty_print(name, bipartite=False, tfidf=False, params={}):
-    """Pretty print a predictor name"""
-    retval = name
-    if bipartite:
-        retval += " bipartite"
-    if tfidf:
-        retval += " TF-IDF"
-    if not params:
-        return retval
+def filter_low_degree_nodes(networks, minimum=1, eligible=None):
+    """Only retain nodes with minimum degree in all networks
 
-    pretty_params = ", ".join("%s = %s" % (k, str(v))
-                              for k, v in params.iteritems())
-    return "%s (%s)" % (retval, pretty_params)
-
-
-def to_tfidf(G):
-    """TF-IDF transform the edges of G
-
-    This is done by transforming its adjacency matrix and then converting back
-    to a network.
-    """
-    import networkx as nx
-    from linkpred.matrix import tfidf_matrix
-    from linkpred.network import from_biadjacency_matrix
-
-    assert nx.is_bipartite(G)
-    row_items = [n for n in G.nodes_iter() if G.node[n]['eligible']]
-    col_items = [n for n in G.nodes_iter() if not G.node[n]['eligible']]
-    matrix = nx.bipartite.biadjacency_matrix(G, row_items, col_items)
-    matrix = tfidf_matrix(matrix)
-    G2 = from_biadjacency_matrix(matrix, row_items, col_items)
-    G2.node = G.node
-    return G2
-
-
-def do_predict(G, predictortype, label, eligible=None, only_new=False, **kwargs):
-    log.logger.info("Executing %s..." % label)
-    predictor = predictortype(G, eligible=eligible, only_new=only_new)
-    scoresheet = predictor(**kwargs)
-    log.logger.info("Finished executing %s." % label)
-    return scoresheet
-
-
-def predict(training, profile, only_new=False, eligible=None):
-    """Generator that yields predictions on the basis of training
+    This also removes nodes that are not present in all networks.
+    Changes are made in place.
 
     Arguments
     ---------
-    training : a Result
-        Training data
+    networks : a list or iterable of networkx.Graph instances
+
+    minimum : int
+        minimum node degree
+
+    """
+    def low_degree(G, threshold):
+        """
+        Find low-degree nodes
+
+        Parameters
+        ----------
+        threshold : int
+            Only nodes whose degree is below the threshold are retained
+
+        """
+        if eligible is None:
+            return [n for n, d in G.degree_iter() if d < threshold]
+        else:
+            return [n for n, d in G.degree_iter()
+                    if d < threshold and G.node[n][eligible]]
+
+    def items_outside(G, container):
+        if eligible is None:
+            return [n for n in G.nodes_iter() if n not in container]
+        else:
+            return [n for n in G.nodes_iter()
+                    if G.node[n][eligible] and n not in container]
+
+    log.logger.info("Filtering low degree nodes...")
+    for G in networks:
+        to_remove = low_degree(G, minimum)
+        G.remove_nodes_from(to_remove)
+        log.logger.info("Removed %d items" % len(to_remove))
+    common = set.intersection(*[set(G) for G in networks])
+    for G in networks:
+        to_remove = items_outside(G, common)
+        G.remove_nodes_from(to_remove)
+        log.logger.info("Removed %d items" % len(to_remove))
+    log.logger.info("Finished filtering low degree nodes.")
+
+
+def pretty_print(name, params={}):
+    """Pretty print a predictor name"""
+    if not params:
+        return name
+
+    pretty_params = ", ".join("%s = %s" % (k, str(v))
+                              for k, v in params.iteritems())
+    return "%s (%s)" % (name, pretty_params)
+
+
+def predict(G, predictortype, eligible=None, only_new=False, **kwargs):
+    predictor = predictortype(G, eligible=eligible, only_new=only_new)
+    scoresheet = predictor(**kwargs)
+    return scoresheet
+
+
+def predict_all(G, profile, only_new=False, eligible=None):
+    """Generator that yields predictions on the basis of training network G
+
+    Arguments
+    ---------
+    G : a networkx.Graph
+        Training network
 
     profile : a dict
         Profile detailing which predictors should be used
 
     only_new : True|False
-        Whether or not we should restrict ourselves to predicting only new links
+        Whether or not we restrict ourselves to predicting only new links
 
     eligible : a string or None
-        If a string, the attribute according to which 'eligible' nodes are found.
-        If None, this is ignored.
+        If a string, the attribute according to which 'eligible' nodes are
+        found. If None, this is ignored.
 
     Returns
     -------
@@ -96,33 +114,21 @@ def predict(training, profile, only_new=False, eligible=None):
 
     """
     for predictor_profile in profile['predictors']:
-        bipartite = predictor_profile.get('bipartite', False)
-        tfidf = predictor_profile.get('tfidf', False)
-        parameters = predictor_profile.get('parameters', {})
+        params = predictor_profile.get('parameters', {})
         name = predictor_profile['name']
         predictortype = getattr(predictors, name)
         label = predictor_profile.get('displayname',
-                                      pretty_print(name, bipartite, tfidf, parameters))
+                                      pretty_print(name, params))
 
-        if bipartite and tfidf:
-            # Create a reusable TF-IDF network, so we don't have to do this
-            # transformation for each predictor.
-            if not hasattr(predict, 'tfidf_network'):
-                predict.tfidf_network = to_tfidf(training.pathspec)
-            G = predict.tfidf_network
-        elif bipartite:
-            G = training.pathspec
-        else:
-            G = training.network
-
-        scoresheet = do_predict(
-            G, predictortype, label, eligible, only_new, **parameters)
+        log.logger.info("Executing %s..." % label)
+        scoresheet = predict(G, predictortype, label, eligible,
+                             only_new, **params)
+        log.logger.info("Finished executing %s." % label)
 
         yield label, scoresheet
 
 
 def connect_signals(listeners):
-    from linkpred.evaluation import signals
     for listener in listeners:
         signals.new_evaluation.connect(listener.on_new_evaluation)
         signals.datagroup_finished.connect(listener.on_datagroup_finished)
@@ -131,15 +137,14 @@ def connect_signals(listeners):
 
 
 def evaluate(datasets, name, filetype="pdf", interpolate=True, steps=1):
-    import linkpred.evaluation.listeners as l
     # TODO figure out easy way to specify which listeners we want
-    cache = l.CachingListener()
-    rp = l.RecallPrecisionPlotter(name, filetype=filetype,
-                                  interpolate=interpolate)
-    f = l.FScorePlotter(name, filetype=filetype, xlabel="# predictions",
-                        steps=steps)
-    roc = l.ROCPlotter(name, filetype=filetype)
-    fmax = l.FMaxListener(name)
+    cache = listeners.CachingListener()
+    rp = listeners.RecallPrecisionPlotter(name, filetype=filetype,
+                                          interpolate=interpolate)
+    f = listeners.FScorePlotter(name, filetype=filetype,
+                                xlabel="# predictions", steps=steps)
+    roc = listeners.ROCPlotter(name, filetype=filetype)
+    fmax = listeners.FMaxListener(name)
     connect_signals((cache, rp, f, roc, fmax))
 
     comp = Comparison()
