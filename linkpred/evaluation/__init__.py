1453c866345c25d0cba64280e1d78fc91bf6ae32
Raf Guns <rafguns@gmail.com>
2013-10-23 11:45:17 +0200

    Remove unused file comparison.py

--- linkpred/evaluation/__init__.py
+++ linkpred/evaluation/__init__.py
@@ -1,6 +1,5 @@
 """Module for evaluating link prediction results"""
 from . import listeners
 from . import signals
-from .comparison import DataSet
 from .scoresheet import *
 from .static import StaticEvaluation
