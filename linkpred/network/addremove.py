efd17f8ffe133b2990e24f09c7c9b2ab75bfbde0
Raf Guns <rafguns@gmail.com>
2014-06-16 11:39:01 +0200

    Optimize addremove procedures

--- linkpred/network/addremove.py
+++ linkpred/network/addremove.py
@@ -1,8 +1,14 @@
-from random import sample
+import random
+
 from ..util import all_pairs, log
 
 __all__ = ['add_random_edges', 'remove_random_edges',
            'add_remove_random_edges']
+
+
+def assert_is_percentage(pct):
+    if not 0 <= pct <= 1:
+        raise ValueError("Percentage should be float between 0 and 1")
 
 
 def add_random_edges(G, pct):
@@ -16,13 +22,13 @@ def add_random_edges(G, pct):
     pct : float
         A percentage (between 0 and 1)
     """
-    edges = G.edges()
-    m = len(edges)
+    assert_is_percentage(pct)
+    m = G.size()
     to_add = int(m * pct)
     log.logger.debug("Will add %d edges to %d (%f)" % (to_add, m, pct))
 
-    new_edges = set(all_pairs(G.nodes())) - set(edges)
-    G.add_edges_from(sample(new_edges, to_add), weight=1)
+    new_edges = set(all_pairs(G)) - set(G.edges_iter())
+    G.add_edges_from(random.sample(new_edges, to_add), weight=1)
 
 
 def remove_random_edges(G, pct):
@@ -36,12 +42,13 @@ def remove_random_edges(G, pct):
     pct : float
         A percentage (between 0 and 1)
     """
+    assert_is_percentage(pct)
     edges = G.edges()
     m = len(edges)
     to_remove = int(m * pct)
 
     log.logger.debug("Will remove %d edges of %d (%f)" % (to_remove, m, pct))
-    G.remove_edges_from(sample(edges, to_remove))
+    G.remove_edges_from(random.sample(edges, to_remove))
 
 
 def add_remove_random_edges(G, pct_add, pct_remove):
@@ -58,14 +65,16 @@ def add_remove_random_edges(G, pct_add, pct_remove):
     pct_remove : float
         A percentage (between 0 and 1)
     """
+    assert_is_percentage(pct_add)
+    assert_is_percentage(pct_remove)
     edges = G.edges()
     m = len(edges)
     to_add = int(m * pct_add)
     to_remove = int(m * pct_remove)
     log.logger.debug("Will add %d (%f) edges to and remove"
-                     "%d (%f) edges from %d" %
+                     "%d (%f) edges of %d" %
                      (to_add, pct_add, to_remove, pct_remove, m))
 
-    new_edges = set(all_pairs(G.nodes())) - set(edges)
-    G.remove_edges_from(sample(edges, to_remove))
-    G.add_edges_from(sample(new_edges, to_add))
+    new_edges = set(all_pairs(G)) - set(edges)
+    G.remove_edges_from(random.sample(edges, to_remove))
+    G.add_edges_from(random.sample(new_edges, to_add))
---
+++ linkpred/network/tests/test_addremove.py
@@ -0,0 +1,47 @@
+import networkx as nx
+from linkpred.network.addremove import *
+from nose.tools import *
+
+
+def test_add_random_edges():
+    G = nx.star_graph(10)
+    edges = G.edges()
+
+    add_random_edges(G, 0)
+    assert_equal(edges, G.edges())
+
+    add_random_edges(G, 0.5)
+    assert_equal(G.size(), 15)
+    assert set(edges) < set(G.edges())
+
+    assert_raises(ValueError, add_random_edges, G, 1.2)
+
+
+def test_remove_random_edges():
+    G = nx.star_graph(10)
+    edges = G.edges()
+
+    remove_random_edges(G, 0)
+    assert_equal(edges, G.edges())
+
+    remove_random_edges(G, 0.5)
+    assert_equal(G.size(), 5)
+    assert set(G.edges()) < set(edges)
+
+    assert_raises(ValueError, remove_random_edges, G, 10)
+
+
+def test_add_remove_random_edges():
+    G = nx.star_graph(10)
+    edges = G.edges()
+
+    add_remove_random_edges(G, 0, 0)
+    assert_equal(edges, G.edges())
+
+    add_remove_random_edges(G, 0.3, 0.4)
+    assert_equal(G.size(), 9)
+    assert_equal(len(set(edges).intersection(set(G.edges()))), 6)
+
+    assert_raises(ValueError, add_remove_random_edges, G, 0, 1.2)
+    assert_raises(ValueError, add_remove_random_edges, G, 1.2, 0)
+
