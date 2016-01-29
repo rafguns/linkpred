7cd1ae86d304234ee1bd54ecd6940c52ffdbd5b5
Raf Guns <rafguns@gmail.com>
2016-01-29 14:55:50 +0100

    Make FT work on Python < 3.5

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -8,8 +8,9 @@ class TestFunctional:
     def test_simple_run(self):
         num_files = len(os.listdir('examples'))
 
-        subprocess.run('python scripts/linkpred examples/inf1990-2004.net '
-                       'examples/inf2005-2009.net -p CommonNeighbours'.split(),
-                       check=True, stdout=subprocess.DEVNULL)
+        subprocess.check_call(
+            'python scripts/linkpred examples/inf1990-2004.net '
+            'examples/inf2005-2009.net -p CommonNeighbours'.split(),
+            stdout=subprocess.DEVNULL)
 
         assert_equal(len(os.listdir('examples')), num_files + 1)
