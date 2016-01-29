997e18e962d008a91c3c65a8507a4dce33580ec9
Raf Guns <rafguns@gmail.com>
2016-01-29 15:29:13 +0100

    Set proper MPL backend with env variable

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -5,11 +5,9 @@ from nose.tools import assert_equal
 
 
 def test_simple_run():
-    import matplotlib
-    matplotlib.use('Agg')
-
     num_files = len(os.listdir('examples'))
 
+    os.putenv('MPLBACKEND', 'Agg')
     subprocess.check_call(
         'python scripts/linkpred examples/inf1990-2004.net '
         'examples/inf2005-2009.net -p CommonNeighbours --quiet'.split())
