cc0f5652fb12535c3a98f77106f76d36b8f542ae
Raf Guns <rafguns@gmail.com>
2016-01-29 15:06:36 +0100

    Shut up matplotlib

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -1,15 +1,20 @@
 import os
 import subprocess
 
-from nose.tools import assert_equal
+from nose.tools import assert_equal, with_setup
 
 
-class TestFunctional:
-    def test_simple_run(self):
-        num_files = len(os.listdir('examples'))
+def setup_mpl():
+    import matplotlib
+    matplotlib.use('Agg')
 
-        subprocess.check_call(
-            'python scripts/linkpred examples/inf1990-2004.net '
-            'examples/inf2005-2009.net -p CommonNeighbours -q'.split())
 
-        assert_equal(len(os.listdir('examples')), num_files + 1)
+@with_setup(setup_mpl)
+def test_simple_run():
+    num_files = len(os.listdir('examples'))
+
+    subprocess.check_call(
+        'python scripts/linkpred examples/inf1990-2004.net '
+        'examples/inf2005-2009.net -p CommonNeighbours --quiet'.split())
+
+    assert_equal(len(os.listdir('examples')), num_files + 1)
